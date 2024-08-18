9b56bc2b26c4aaf58cf08da7a2d34383635fa683
Bhavikg27 <super27dancer@gmail.com>
2024-08-18 16:20:08 +0530

    added other sort codes

--- src/learning_java/sorting.java
+++ src/learning_java/sorting.java
@@ -9,21 +9,56 @@ public class sorting {
         System.out.println();
     }
 
-    public static void bubblesort(int arr[]){
-        for(int i=0;i< arr.length-1; i++){
-            for(int j=0;j< arr.length-i-1;j++){
-                if(arr[j]>arr[j+1]){
-                    int temp=arr[j];
-                    arr[j]=arr[j+1];
-                    arr[j+1]=temp;
+    //time complexity = 0(n^2)
+    public static void bubblesort(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
             }
         }
         printarr(arr);
     }
 
+    //time complexity = 0(n^2)
+    public static void selectionsort(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int smallest = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[smallest] > arr[j]) {
+                    smallest = j;
+                }
+            }
+            int temp = arr[smallest];
+            arr[smallest] = arr[i];
+            arr[i] = temp;
+        }
+        printarr(arr);
+    }
+
+    public static void insertionsort(int arr[]) {
+        for (int i = 1; i < arr.length; i++) {
+            int current = arr[i];
+            int j = i - 1;
+            while (j >= 0 && current < arr[j]){
+                arr[j+1]=arr[j];
+                j--;
+            }
+
+            //placement
+            arr[j+1] = current;
+        }
+        printarr(arr);
+    }
+
+
     public static void main(String[] args) {
-        int arr[]={3,7,1,9,5};
+        int arr[] = {3, 7, 1, 9, 5};
         bubblesort(arr);
+        selectionsort(arr);
+        insertionsort(arr);
     }
 }
