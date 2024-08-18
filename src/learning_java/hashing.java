d8b0e29ebd497794e1ed2429e039039672baa0a8
Bhavikg27 <super27dancer@gmail.com>
2024-08-18 12:27:34 +0530

    start of github

---
+++ src/learning_java/hashing.java
@@ -0,0 +1,16 @@
+package learning_java;
+
+import java.util.HashSet;
+
+public class hashing {
+    public static void main(String[] args) {
+        HashSet<Integer> myHashSet = new HashSet<>(6,0.5f);
+        myHashSet.add(6);
+        myHashSet.add(8);
+        myHashSet.add(1);
+        myHashSet.add(9);
+        myHashSet.add(5);
+        myHashSet.add(19);
+        System.out.println(myHashSet);
+    }
+}
