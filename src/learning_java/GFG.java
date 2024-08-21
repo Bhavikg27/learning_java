8563b7eb2275e70ca945569b9bb83c6c79dfdaad
Bhavikg27 <super27dancer@gmail.com>
2024-08-21 23:47:32 +0530

    testing

--- src/learning_java/GFG.java
+++ src/learning_java/GFG.java
@@ -1,12 +1,12 @@
-class Test {
-    int x;
+package learning_java;
 
-    Test(int x) {
-        this.x = x;
-    }
-
+class GFG{
     public static void main(String[] args) {
-        Test obj = new Test(5);
-        System.out.println(obj.x);
+    int a = 'a';
+    System.out.println(a);
+
+    int asciiValue = 97;
+    char character = (char) asciiValue;
+    System.out.println(character);
     }
 }
