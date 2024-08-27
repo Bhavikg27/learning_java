f67afbe11c3751c5202bfb5e29c410a6a0423393
Bhavikg27 <super27dancer@gmail.com>
2024-08-28 00:27:41 +0530

    exercise 7

---
+++ src/learning_java/ex7.java
@@ -0,0 +1,7 @@
+package learning_java;
+
+public class ex7 {
+    public static void main(String[] args) {
+
+    }
+}
--- src/learning_java/exercise_4.java
+++ src/learning_java/exercise_4.java
@@ -1,23 +1,22 @@
-package learning_java.learning_java;
+package learning_java;
 
-import java.util.*;
 class library{
-    String[] issuedbooks;
+    String[] IssuedBooks;
     String[] books;
     int no_of_books;
-    int no_of_issuedbooks;
+    int no_of_IssuedBooks;
     library(){
         this.books= new String[100];
-        this.issuedbooks=new String[100];
+        this.IssuedBooks =new String[100];
         this.no_of_books=0;
-        this.no_of_issuedbooks=0;
+        this.no_of_IssuedBooks =0;
     }
     void AddBook(String book){
         this.books[no_of_books]=book;
         System.out.println(book+" has been added.");
         no_of_books++;
     }
-    void showavailablebooks(){
+    void ShowAvailableBooks(){
         System.out.println("available books are - ");
         for (String book:this.books) {
             if(book==null){
@@ -26,7 +25,7 @@ class library{
             System.out.println("* "+book);
         }
     }
-    void totalnoofbboks(){
+    void TotalNoOfBooks(){
         int count = 0;
         for (String book : books) {
             if (book != null) {
@@ -35,34 +34,34 @@ class library{
         }
         System.out.println(count);
     }
-    void issuebook(String book) {
+    void IssueBook(String book) {
         for (int i = 0; i < this.books.length; i++) {
             if (this.books[i] == null) {
                 continue;
             } else if (this.books[i].equalsIgnoreCase(book)) {
                 this.books[i] = null;
-                // Check if the slot in issuedbooks is null before assigning
-                for (int j = 0; j < this.issuedbooks.length; j++) {
-                    if (this.issuedbooks[j] == null) {
-                        this.issuedbooks[j] = book;
+                // Check if the slot in IssuedBooks is null before assigning
+                for (int j = 0; j < this.IssuedBooks.length; j++) {
+                    if (this.IssuedBooks[j] == null) {
+                        this.IssuedBooks[j] = book;
                         break;
                     }
                 }
                 System.out.println(book + " has been issued");
-                no_of_issuedbooks++;
+                no_of_IssuedBooks++;
                 return;
             }
         }
         System.out.println("This book is currently not in the library");
     }
-    void returnbook(String book) {
+    void ReturnBook(String book) {
         System.out.println(book + " has been returned.");
-        for (int i = 0; i < this.issuedbooks.length; i++) {
-            if (this.issuedbooks[i] == null) {
+        for (int i = 0; i < this.IssuedBooks.length; i++) {
+            if (this.IssuedBooks[i] == null) {
                 continue;
             }
-            if (this.issuedbooks[i].equalsIgnoreCase(book)) {
-                this.issuedbooks[i] = null;
+            if (this.IssuedBooks[i].equalsIgnoreCase(book)) {
+                this.IssuedBooks[i] = null;
                 this.books[no_of_books] = book.toUpperCase();
                 no_of_books++;
                 return;
@@ -70,9 +69,9 @@ class library{
         }
         System.out.println("This book was not issued from the library");
     }
-    void showissuedbooks(){
+    void ShowIssuedBooks(){
         System.out.println("issued books are - ");
-        for (String book:this.issuedbooks) {
+        for (String book:this.IssuedBooks) {
             if(book==null){
                 continue;
             }
@@ -89,15 +88,15 @@ public class exercise_4 {
         central_library.AddBook("IRON MAN2");
         central_library.AddBook("IRON MAN3");
         central_library.AddBook("IRON MAN4");
-        central_library.showavailablebooks();
-        central_library.totalnoofbboks();
-        central_library.issuebook("iron man2");
-        central_library.issuebook("iron man4");
-        central_library.showavailablebooks();
-        central_library.showissuedbooks();
-        central_library.returnbook("iron man2");
-        central_library.returnbook("iron man4");
-        central_library.showavailablebooks();
-        central_library.showissuedbooks();
+        central_library.ShowAvailableBooks();
+        central_library.TotalNoOfBooks();
+        central_library.IssueBook("iron man2");
+        central_library.IssueBook("iron man4");
+        central_library.ShowAvailableBooks();
+        central_library.ShowIssuedBooks();
+        central_library.ReturnBook("iron man2");
+        central_library.ReturnBook("iron man4");
+        central_library.ShowAvailableBooks();
+        central_library.ShowIssuedBooks();
     }
 }
