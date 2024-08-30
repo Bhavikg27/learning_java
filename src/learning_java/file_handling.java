216638fb365b7b31fb705dae152a6015d84f829d
Bhavikg27 <super27dancer@gmail.com>
2024-08-31 02:50:14 +0530

    file handling

---
+++ src/learning_java/file_handling.java
@@ -0,0 +1,56 @@
+package learning_java;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class file_handling {
+    public static void main(String[] args) {
+        /*
+//      code to create a new File
+
+        File myFile = new File("bhavik.txt");
+        try {
+            myFile.createNewFile();
+        } catch (IOException e) {
+            System.out.println("unable to create this file");
+            throw new RuntimeException(e);
+        }
+
+//      code to write to a file
+
+        try {
+            FileWriter fileWriter = new FileWriter("bhavik.txt");
+            fileWriter.write("this is our first file from this java course\nbye bye!!");
+            fileWriter.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+//      code to read a file
+
+        File myFile = new File("bhavik.txt");
+        try {
+            Scanner sc = new Scanner(myFile);
+            while(sc.hasNextLine()){
+                String line  = sc.nextLine();
+                System.out.println(line);
+            }
+            sc.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+//      code for deleting a file
+
+        File myFile = new File("bhavik.txt");
+        if(myFile.delete()){
+            System.out.println("I have deleted: "+ myFile.getName());
+        }else{
+            System.out.println("some error occurred while deleting the file");
+        }
+        */
+    }
+}
