755067c88c10c84bc2875445b400b6c1102d127c
Bhavikg27 <super27dancer@gmail.com>
2024-09-13 14:00:14 +0530

    gfg streak 3

--- src/learning_java/GFG.java
+++ src/learning_java/GFG.java
@@ -1,45 +1,106 @@
-package learning_java;
+package learning_java;//{ Driver Code Starts
+// Initial Template for Java
 
-//{ Driver Code Starts
+// Contributed by Sudarshan Sharma
+import java.util.LinkedList;
+import java.util.Queue;
 import java.io.*;
 import java.util.*;
 
 class Node {
     int data;
-    Node next;
-
-    Node(int x) {
-        data = x;
-        next = null;
+    Node left;
+    Node right;
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
     }
 }
 
-class GFG {
-    static void printList(Node node) {
-        while (node != null) {
-            System.out.print(node.data + " ");
-            node = node.next;
+class GfG {
+
+    static Node buildTree(String str) {
+
+        if (str.length() == 0 || str.charAt(0) == 'N') {
+            return null;
         }
-        System.out.println();
+
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+
+        Queue<Node> queue = new LinkedList<>();
+
+        queue.add(root);
+        // Starting from the second element
+
+        int i = 1;
+        while (queue.size() > 0 && i < ip.length) {
+
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+
+            // Get the current node's value from the string
+            String currVal = ip[i];
+
+            // If the left child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+
+            // For the right child
+            i++;
+            if (i >= ip.length) break;
+
+            currVal = ip[i];
+
+            // If the right child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+    void inOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+
+        inOrder(node.left);
+        System.out.print(node.data + " ");
+
+        inOrder(node.right);
     }
 
-    public static void main(String args[]) throws IOException {
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine());
-        while (t > 0) {
-            String str[] = read.readLine().trim().split(" ");
-            int n = str.length;
-            Node head = new Node(Integer.parseInt(str[0]));
-            Node tail = head;
-            for (int i = 1; i < n; i++) {
-                tail.next = new Node(Integer.parseInt(str[i]));
-                tail = tail.next;
-            }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+                new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            String s = br.readLine();
+            Node root = buildTree(s);
+
             Solution g = new Solution();
-            int ans = g.getMiddle(head);
-            System.out.println(ans);
-            // printList(head);
-            t--;
+            g.mirror(root);
+            GfG mt = new GfG();
+            mt.inOrder(root);
+            System.out.println();
         }
     }
 }
@@ -47,22 +108,32 @@ class GFG {
 // } Driver Code Ends
 
 
-/* Node of a linked list
- class Node {
-   int data;
-    Node next;
-    Node(int d)  { data = d;  next = null; }
-}
-*/
+// User function Template for Java
+
+// function Template for Java
+
+// FUNCTION CODE
+/* A Binary Tree node
+class Node
+{
+    int data;
+    Node left, right;
+   Node(int item)
+   {
+        data = item;
+        left = right = null;
+    }
+} */
 
 class Solution {
-    int getMiddle(Node head) {
-        Node slow = head;
-        Node fast = head;
-        while(fast != null && fast.next!=null){
-            slow=slow.next;
-            fast=fast.next.next;
-        }
-        return slow.data;
+    // Function to convert a binary tree into its mirror tree.
+    void mirror(Node node) {
+        mirror(node.left);
+        mirror(node.right);
+
+        // Swap the left and right children
+        Node temp = node.left;
+        node.left = node.right;
+        node.right = temp;
     }
 }
