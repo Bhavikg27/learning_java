2f79bf78e5bbd63951e91817e1bc064c55e0106c
Bhavikg27 <super27dancer@gmail.com>
2024-09-19 01:45:55 +0530

    will do it later

--- src/learning_java/N_Queen.java
+++ src/learning_java/N_Queen.java
@@ -1,18 +1,5 @@
 package learning_java;
 
-import java.util.ArrayList;
-import java.util.List;
-
-class Solution1{
-    public List<List<String>> solveNQueens(int n){
-        List<List<String>> allBoards = new ArrayList<>();
-        char [][] board = new board[n][n];
-
-        //will do later
-
-    }
-
-}
 public class N_Queen {
     public static void main(String[] args) {
 
